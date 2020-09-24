2e63cf4a7223f2803bda7199f04fc8178cdd7492
Catie Poggioli <cmp5987@rit.edu>
2020-09-24 16:53:33 -0400

    Initial Code for Login and Register Page

---
+++ app/src/main/java/edu/rit/cmp5987/kotlinmessanger/LoginActivity.kt
@@ -0,0 +1,17 @@
+package edu.rit.cmp5987.kotlinmessanger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //set layout
+        setContentView(R.layout.activity_login)
+
+    }
+
+
+}
